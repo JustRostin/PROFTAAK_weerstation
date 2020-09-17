60f7fb4f97a57071c26d9e7e05bf9c94caae1b6f
Raoul de Heer <raouldeheer@users.noreply.github.com>
2020-09-17 09:58:44 +0200

    Create ValueConverter.java

---
+++ src/com/avans/ValueConverter.java
@@ -0,0 +1,125 @@
+package com.avans;
+
+public class ValueConverter {
+
+    /**
+     * airPressure
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De luchtdruk in hPa
+     */
+    public static double airPressure(short rawValue) {
+
+    }
+
+
+
+    /**
+     * temperature
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De temperatuur in graden Celcius
+     */
+    public static double temperature(short rawValue) {
+
+    }
+
+
+
+    /**
+     * humidity
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De relatieve luchtvochtigheid in procenten
+     */
+    public static double humidity(short rawValue) {
+
+    }
+
+
+
+    /**
+     * windSpeed
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De windsnelheid in km/h
+     */
+    public static double windSpeed(short rawValue) {
+
+    }
+
+
+
+    /**
+     * windDirection
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De windrichting in graden
+     */
+    public static double windDirection(short rawValue) {
+
+    }
+
+
+
+    /**
+     * rainMeter
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De hoeveelheid regen in mm
+     */
+    public static double rainMeter(short rawValue) {
+
+    }
+
+
+
+    /**
+     * uvIndex
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De uv index (dimensieloos)
+     */
+    public static double uvIndex(short rawValue) {
+
+    }
+
+
+
+    /**
+     * batteryLevel
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return De batterijspanning in Volt
+     */
+    public static double batteryLevel(short rawValue) {
+
+    }
+
+
+
+    /**
+     * sunRise
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return Zonsopkomst in hh:mm notatie
+     */
+    public static String sunRise(short rawValue) {
+
+    }
+
+
+
+    /**
+     * sunSet
+     *
+     * @param rawValue Ruwe meetwaarde van het vp2pro weerstation
+     * @return Zonsondergang in hh:mm notatie
+     */
+    public static String sunSet(short rawValue) {
+
+    }
+
+
+
+}
